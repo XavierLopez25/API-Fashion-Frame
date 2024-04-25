7268900817657812c5cd239086cf92a32e611d1d
XavierLopez25 <lop22716@uvg.edu.gt>
2024-04-25 17:44:11 -0600

    feat: added main.js file for API

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'bodyparser'
+
+const app = express();
+app.use(express.json);
+app.use(bodyParser.json);
+app.use(cors());
+
+const port = 5001;
+
+app.get('/', async(req, res) => {
+    res.send('Hello World from API.')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening at http://127.0.0.1:${port}`);
+})
